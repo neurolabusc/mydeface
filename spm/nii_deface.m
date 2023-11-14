211741b5573dff1ddc1bea5251eaa6c9a55e1f01
neurolabusc <rorden@sc.edu>
2023-11-14 16:01:31 -0500

    Add overwrite

--- spm/nii_deface.m
+++ spm/nii_deface.m
@@ -1,4 +1,4 @@
-function names = nii_deface(job)
+function names = nii_deface(job, overwrite)
 % extends nii_deface to remove excess neck and face details
 % Face strip images
 % FORMAT names = spm_deface(job)
@@ -22,14 +22,17 @@ elseif isstruct(job)
 else
     P = job;
 end
+if ~exist('overwrite','var') 
+    overwrite = false;
+end
 P = cellstr(P);
 names = cell(size(P));
 tpm = spm_load_priors8(spm_vol(fullfile(spm('Dir'),'tpm','TPM.nii')));
 for i=1:numel(P)
-    names{i} = deface(P{i},tpm);
+    names{i} = deface(P{i},tpm, overwrite);
 end
 
-function fname = deface(P,tpm)
+function fname = deface(P,tpm, overwrite)
 M       = spm_maff8(P,4,20,tpm,[],'mni');
 Nii     = nifti(P);
 d       = [size(Nii.dat) 1];
@@ -54,21 +57,37 @@ msk2     = nul1(1)*i + nul1(2)*j + nul1(3)*k + nul1(4) < 0;
 msk = max(msk, msk2);
 % create two masks to remove jaw
 % msk inferior removes signal near feet
-nul     = [0 0 1 45];
+nul     = [0 -0.2 1 50];
 nul1    = nul*M*Nii.mat;
 mskInferior     = nul1(1)*i + nul1(2)*j + nul1(3)*k + nul1(4) < 0;
 % msk anterior removes signal near the nose
-nul     = [0 -1.3 0.8 60];
+nul     = [0 -1.0 0.8 75];
 nul1    = nul*M*Nii.mat;
 mskAnterior     = nul1(1)*i + nul1(2)*j + nul1(3)*k + nul1(4) < 0;
 % remove intersection of foot and nose removal
 msk2 = min(mskInferior, mskAnterior);
 % combine compound jaw mask to others:
 msk = max(msk, msk2);
+% next masks are optional, but remove phantom face signal and excess neck
+% add a plane to rem
+% ove posterior
+nul     = [0 1 0 120];
+nul1    = nul*M*Nii.mat;
+msk2     = nul1(1)*i + nul1(2)*j + nul1(3)*k + nul1(4) < 0;
+msk = max(msk, msk2);
+% add a plane to remove superior to head
+nul     = [0 0 1 105];
+nul1    = nul*M*Nii.mat;
+msk2     = nul1(1)*i + nul1(2)*j + nul1(3)*k + nul1(4) < 0;
+msk = max(msk, msk2);
 
 % Ensure anything that may reveal identity is not included in the face-stripped
 % version. This includes hidden fields, extensions etc that may contain strings.
+
+%fname   = Nii.dat.fname;
+%fname   = spm_file(Nii.dat.fname,'prefix','anon_');
 fname   = spm_file(Nii.dat.fname,'prefix','anon_');
+
 %Noo    = Nii; % This was unsafe because Nii may contain hidden strings
 Noo             = nifti;
 Noo.dat         = file_array(fname, Nii.dat.dim, Nii.dat.dtype, 0, ...
@@ -93,3 +112,9 @@ for k=1:size(Noo.dat,6)
         end
     end
 end
+if overwrite
+    [p,n,x] = fileparts(Nii.dat.fname);
+    movefile(Nii.dat.fname,fullfile(p,['_',n,x]));
+    movefile(fullfile(p,['anon_',n,x]), Nii.dat.fname);
+end
+
